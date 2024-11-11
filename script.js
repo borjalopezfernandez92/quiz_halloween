8191284b4889da9bd4573a98a2c08b827a5a04e8
borjalopezfernandez92 <borjalopezfernandez92@gmail.com>
2024-11-11 20:47:47 +0100

    11/NOV funcional hasta comprobación de respuesta errónea o correcta

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
+// Decodificar html << buscar para solucionar formato de comilla
+
 createFetch();
 
-function test() {
-    alert("test");
-}
-
-
-// function paintDifficulty() {
-//     console.log("test paintDifficulty");
-//     let form = document.createElement("form");
-//     form.id = "formDifficulty";
-//     let radioFacil = document.createElement("input");
-//     radioFacil.type = "radio";
-//     radioFacil.name = "difficulty";
-//     radioFacil.id = "radioFacilId";
-//     let facilText = document.createElement("label");
-//     facilText.name = "difficulty";
-//     facilText.innerText = "Fácil";
-
-//     let getButtonsDiv = document.getElementById("ButtonsDivId");
-//     getButtonsDiv.appendChild(facilText);
-//     getButtonsDiv.appendChild(radioFacil);
-// }
 
 function paintResults() {
     console.log("test paintResults");
@@ -49,79 +30,93 @@ function fetchQuestion() { // realiza el fetch
 
 
 function checkFetch(fetch) {
+    let getBodyDivQuestions = document.createElement("div");
+    getBodyDivQuestions.id = "hiddenDiv";
+
     for (let i = 0; i < fetch.results.length; i++) {
-        // localStorage.setItem(`pregunta${i}`, fetch.results[i].correct_answer)
-        // console.log(fetch.results[i].correct_answer);
-        let getBodyDivQuestions = document.getElementById("divShowQuestions");
         let pregunta = document.createElement("div");
         pregunta.innerText = fetch.results[i].question;
         pregunta.id = "pregunta" + [i];
         pregunta.className = "hidden";
         getBodyDivQuestions.appendChild(pregunta);
+        document.body.appendChild(getBodyDivQuestions);
 
         // Pintado de opciones incorrectas
         let wrongOptions = fetch.results[i].incorrect_answers;
-        manageOptions(wrongOptions);
+        manageOptions(wrongOptions, i);
 
 
         // Pintado de opciones correctas
         let correctAnswer = document.createElement("div");
         correctAnswer.innerText = fetch.results[i].correct_answer;
-        correctAnswer.id = "opcion4";
+        correctAnswer.id = `vulta${i}Opcion3`;
         correctAnswer.className = "hidden";
         getBodyDivQuestions.appendChild(correctAnswer);
 
     }
-    manageQuestions();
+    // manageQuestions();
+    paintQuestion(0);
 }
 
-function manageOptions(options) {
+function manageOptions(options, vuelta) {
     for (let i = 0; i < options.length; i++) {
-        let getBodyDivQuestions = document.getElementById("divShowQuestions");
+        let getBodyDivQuestions = document.getElementById("hiddenDiv");
         let option = document.createElement("div");
         // option.onclick()
         option.innerText = options[i];
-        option.id = "opcion" + [i];
+        option.id = `vulta${vuelta}Opcion${i}`;
         option.className = "hidden";
         getBodyDivQuestions.appendChild(option);
         // console.log(options[i]);
     }
 }
 
+function paintQuestion(nPregunta) {
+    // let cleaner = document.getElementById("divShown");
 
-function manageQuestions() {
-    let nPregunta = 0;
+    // if (cleaner) {
 
-    switch (nPregunta) {
-        case 0:
-            console.log("caso 0");
-            let getPregunta0 = document.getElementById("pregunta0");
-            getPregunta0.className = "questionshown";
+    // }
 
-            let getOpcion0 = document.getElementById("opcion0");
-            let getOpcion1 = document.getElementById("opcion1");
-            let getOpcion2 = document.getElementById("opcion2");
-            let getCorrect = document.getElementById("opcion4");
-            getOpcion0.className = "shown";
-            getOpcion1.className = "shown";
-            getOpcion2.className = "shown";
-            getCorrect.className = "shown";
-            paintOptions(getOpcion0, getOpcion1, getOpcion2, getCorrect);
 
-            nPregunta++;
-            break;
+    let getdivShowQuestions = document.getElementById("divShown");
+    let pregunta = document.createElement("p");
+    pregunta.className = "questionshown";
+    pregunta.id = "questionShown";
+    let getPregunta = document.getElementById("pregunta" + nPregunta).innerText;
+    pregunta.innerText = getPregunta;
+    getdivShowQuestions.appendChild(pregunta);
 
-        case 1:
-            console.log("caso 1");
-            let getPregunta1 = document.getElementById("pregunta1");
-            getPregunta1.className = "";
-            nPregunta++;
-            break;
-        default:
-            break;
+
+
+
+    let getOpcion0 = document.getElementById(`vulta${nPregunta}Opcion${nPregunta}`).innerText;
+    let getOpcion1 = document.getElementById(`vulta${nPregunta}Opcion${nPregunta+1}`).innerText;
+    let getOpcion2 = document.getElementById(`vulta${nPregunta}Opcion${nPregunta+2}`).innerText;
+    let getCorrect = document.getElementById(`vulta${nPregunta}Opcion${nPregunta+3}`).innerText;
+    localStorage.setItem("check", getCorrect);
+    let arrayOpciones = [getOpcion0, getOpcion1, getOpcion2, getCorrect];
+    arrayOpciones.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < arrayOpciones.length; i++) {
+        let opcion = document.createElement("p");
+        opcion.id = `opcion${i}`;
+        opcion.className = "shown";
+        opcion.innerText = arrayOpciones[i];
+        let checker = arrayOpciones[i];
+        opcion.setAttribute(`onClick`, `checkCorrect("${checker}")`);
+        getdivShowQuestions.appendChild(opcion);
     }
+    nPregunta++;
 }
 
-function paintOptions(opcion1, opcion2, opcion3, opcion4) {
-    console.log("test paintOptions" + opcion1, opcion2, opcion3, opcion4);
+
+function checkCorrect(opcionEscogida) {
+    let checker = localStorage.getItem("check");
+    if (checker != opcionEscogida) {
+        console.log("Respuesta incorrecta");
+    } else {
+        console.log("Respuesta correcta!");
+        paintQuestion();
+    }
 }
